4d5a2f2d6878d0a07514dbf8c3628584176bd784
harakachi <harakachi@gmail.com>
2013-04-30 15:32:49 +0900

    Initial Commit

---
+++ dotinstall1/MixiAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  MixiAppDelegate.h
+//  dotinstall1
+//
+//  Created by 原田 勝信 on 2013/04/30.
+//  Copyright (c) 2013年 原田 勝信. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MixiAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ dotinstall1/MixiViewController.h
@@ -0,0 +1,13 @@
+//
+//  MixiViewController.h
+//  dotinstall1
+//
+//  Created by 原田 勝信 on 2013/04/30.
+//  Copyright (c) 2013年 原田 勝信. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MixiViewController : UIViewController
+
+@end
